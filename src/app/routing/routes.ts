88674c6316fb40c487593beb49c144f52e11244c
Paweł Białas <p.cz.bialas@gmail.com>
2022-05-03 23:47:18 +0200

    task setup component

--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -3,11 +3,12 @@ import {HomeComponent} from "../views/home/home.component";
 import {LoginComponent} from "../views/login/login.component";
 import {AboutComponent} from "../views/about/about.component";
 import {RegisterComponent} from "../views/register/register.component";
+import {TaskSetupComponent} from "../views/basic-task/task-setup/task-setup.component";
 
 
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: TaskSetupComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent}
