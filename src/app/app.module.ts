5e25acab7c5ab5981bb2b810f9d6e8a2c441f357
pawel-bialas <p.cz.bialas@gmail.com>
2022-05-03 12:08:37 +0200

    added routing module and some basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
+//CoreStuff
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material/material.module";
 import {RouterModule} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+//MyModules
+import {MaterialModule} from "./material/material.module";
 import {ROUTES} from "./routing/routes";
+
+//Components
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { AboutComponent } from './views/about/about.component';
+import { RegisterComponent } from './views/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    AboutComponent
+    AboutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from "../views/home/home.component";
 import {LoginComponent} from "../views/login/login.component";
 import {AboutComponent} from "../views/about/about.component";
+import {RegisterComponent} from "../views/register/register.component";
 
 
 
@@ -9,4 +10,5 @@ export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'login', component: RegisterComponent}
 ];
